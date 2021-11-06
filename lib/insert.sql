116540a5b034785b5c56e17158f61298b10be2a5
Craig <craigjope@gmail.com>
2021-11-05 19:47:26 -0600

    complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (1,'Yogi',5,'M','brown','hungry',TRUE);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (2,'Teddy',8,'F','white','lovable',TRUE);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (3,'Pooh',9,'M','yellow','friendly',TRUE);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (4,'Papa',8,'M','brown','angry',TRUE);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (5,'Mama',9,'F','black','angry',TRUE);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (6,'Baby',2,'F','black','confused',TRUE);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (7,'Soviet',3,'M','white','phlegmatic',TRUE);
+INSERT INTO bears (id,name,age,sex,color,temperament,alive) VALUES (8,null,4,'F','brown','lonely',TRUE);
